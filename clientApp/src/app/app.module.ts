4c2d8e6b392c08df1b4ce8bb307274044851d47b
chronos25 <ritikrajsrivastava90@gmail.com>
2022-01-06 00:45:17 +0530

    added dummy table for simulator

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     MatCardModule,
     MatButtonModule,
+    MatTableModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
