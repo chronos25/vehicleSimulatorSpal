5f6119ef04d2bb4fa79218545d86003abe3e1c05
chronos25 <ritikrajsrivastava90@gmail.com>
2022-01-05 22:57:09 +0530

    added homepage/welcome page

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     MatCardModule,
+    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
